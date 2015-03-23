47004b2790c3ee8a473a773174bd6f9740e93527
Guillem Marpons <gmarpons@gmail.com>
2015-03-23 15:26:28 +0100

    Rename DB table fields

--- BcnOData/Persistent.hs
+++ BcnOData/Persistent.hs
@@ -28,57 +28,57 @@ share
   ]
   [persistLowerCase|
     FontsDeDades
-      taula_i_font       Text        -- Where the data is stored ++ its source,
+      taulaIFont         Text        -- Where the data is stored ++ its source,
                                      -- usually a URL
       tipus              Text        -- E.g. "OData-XML"
-      darrera_descarrega UTCTime     -- SQL type: DATE
+      darreraDescarrega  UTCTime     -- SQL type: DATE
       origen             Text        -- Institution that generated the data set
       comentari          Text        -- E.g. limitations, transformations done.
       url1               Text        -- Pointer to metainfo with, e.g, a
                                      -- description of every field
       url2               Text
-      UniqueTaulaIFont taula_i_font  -- Cannot simply create a composite key
+      UniqueTaulaIFont taulaIFont    -- Cannot simply create a composite key
                                      -- with this as repsert fails and need to
                                      -- use upsert
 
     Districtes
-      codi_districte        Int
-      nom_districte         Text
-      Primary codi_districte
-      UniqueNomDistricte nom_districte
+      codiDistricte        Int
+      nomDistricte         Text
+      Primary codiDistricte
+      UniqueNomDistricte nomDistricte
       deriving Show
 
     Barris
-      codi_barri       Int
-      nom_barri        Text
-      codi_districte   Int Maybe
-      url_fitxa_barri  Text Maybe
-      Primary codi_barri
-      UniqueNomBarri nom_barri
-      Foreign Districtes fkey codi_districte
+      codiBarri        Int
+      nomBarri         Text
+      codiDistricte    Int Maybe
+      urlFitxaBarri    Text Maybe
+      Primary codiBarri
+      UniqueNomBarri nomBarri
+      Foreign Districtes fkey codiDistricte
       deriving Show
 
     MigrantsPerSexe
-      codi_barri       Int
+      codiBarri        Int
       any_             Int
       tipus            Text             sqltype=varchar(1)     -- "I" | "E"
-      codi_districte   Int Maybe
+      codiDistricte    Int Maybe
       dones            Int
       homes            Int
       total            Int
-      Primary codi_barri any_ tipus
-      Foreign Barris fkeyBarri codi_barri
-      Foreign Districtes fkeyDistricte codi_districte
+      Primary codiBarri any_ tipus
+      Foreign Barris fkeyBarri codiBarri
+      Foreign Districtes fkeyDistricte codiDistricte
       deriving Show
 
     AturatsEvolucioMensual
-      codi_barri       Int
+      codiBarri        Int
       any_             Int
       mes              Int
-      codi_districte   Int Maybe
+      codiDistricte    Int Maybe
       num              Int
-      Primary codi_barri any_ mes
-      Foreign Barris fkeyBarri codi_barri
-      Foreign Districtes fkeyDistricte codi_districte
+      Primary codiBarri any_ mes
+      Foreign Barris fkeyBarri codiBarri
+      Foreign Districtes fkeyDistricte codiDistricte
       deriving Show
   |]
